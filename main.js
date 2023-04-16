94c46c3a384a2aacdc9061c53171394c3281bdf7
raphael <rapha.silvaalbuquerque@hotmail.com>
2023-04-16 13:34:05 -0300

    Saving Progress

---
+++ main.js
@@ -0,0 +1,15 @@
+var lamp = window.document.querySelector('#lapagada')
+var lampoff = window.document.querySelector('')
+
+function liga(){
+    
+    lamp.src = './images/acesa.svg'
+}
+
+function desligar(){
+        lamp.src = './images/apagada.svg'
+}
+
+function quebrar(){
+    lamp.src = './images/quebrada.svg'
+}
